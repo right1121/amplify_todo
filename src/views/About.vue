4a3af771feb7280691676010f6a98072e5ebea8d
MsakiHoshino <right1121@hotmail.co.jp>
2020-06-27 01:27:51 +0900

    v-bind実装

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p>{{ msg }}</p>
+    <a v-bind:href="url">GoogleのURL</a>
   </div>
 </template>
 
@@ -9,7 +10,8 @@
 export default {
   data: () => {
     return {
-      msg: 'ここが本文'
+      msg: 'ここが本文',
+      url: 'http://google.com'
     }
   }
 }
