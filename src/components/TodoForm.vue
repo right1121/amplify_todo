1d5893d8735e9f4425312c130cb894745b7cb2fb
MsakiHoshino <right1121@hotmail.co.jp>
2020-07-07 17:43:34 +0900

    Todoフォームコーポネント作成

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="todo-form">
+    <div>
+      <label>名前</label>
+      <input type="text" name="name" id="name">
+    </div>
+    <div>
+      <label>説明</label>
+      <input type="text" name="description" id="description">
+    </div>
+  </div>
+</template>
--- src/views/TodoCreate.vue
+++ src/views/TodoCreate.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="todo-create">
+    <TodoForm/>
     <button>登録</button>
   </div>
 </template>
 
 <script>
+import TodoForm from '@/components/TodoForm.vue'
+
 export default {
-  
+  components: {
+    TodoForm
+  }
 }
 </script>
