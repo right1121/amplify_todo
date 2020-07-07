174da8288642e25409343b5316372e755adf021b
MsakiHoshino <right1121@hotmail.co.jp>
2020-07-08 00:47:51 +0900

    フォーム作成

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,12 +1,41 @@
 <template>
   <div id="todo-form">
     <div>
-      <label>名前</label>
-      <input type="text" name="name" id="name">
+      <label>名前:</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        :value="name"
+        v-on:input="updateName"
+      >
     </div>
     <div>
-      <label>説明</label>
-      <input type="text" name="description" id="description">
+      <label>説明:</label>
+      <input
+        type="text"
+        name="description"
+        id="description"
+        :value="description"
+        v-on:input="updateDescription"
+      >
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    description: String
+  },
+  methods: {
+    updateName: function(e) {
+      this.$emit("updateName", e.target.value)
+    },
+    updateDescription: function(e) {
+      this.$emit("updateDescription", e.target.value)
+    }
+  }
+}
+</script>
--- src/views/TodoCreate.vue
+++ src/views/TodoCreate.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="todo-create">
-    <TodoForm/>
+    <TodoForm
+      :name="name"
+      :description="description"
+      @updateName="updateName"
+      @updateDescription="updateDescription"
+    />
     <button>登録</button>
   </div>
 </template>
@@ -11,6 +16,20 @@ import TodoForm from '@/components/TodoForm.vue'
 export default {
   components: {
     TodoForm
+  },
+  data: function () {
+    return {
+      name: null,
+      description: null
+    }
+  },
+  methods: {
+    updateName(value) {
+      this.name = value
+    },
+    updateDescription(value) {
+      this.description = value
+    },
   }
 }
 </script>
